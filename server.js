a275332767be1fa3449efe345afbfc6c5aec95fa
Tirth Patel <59409218+pappu6@users.noreply.github.com>
2021-04-19 11:24:39 +0530

    server file

---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const path = require('path');
+const cookieSession = require('cookie-session');
+const createError = require('http-errors');
+const bodyParser = require('body-parser');
+
+const FeedbackService = require('./services/FeedbackService');
+const SpeakerService = require('./services/SpeakerService');
+
+const feedbackService = new FeedbackService('./data/feedback.json');
+const speakersService = new SpeakerService('./data/speakers.json');
+
+const routes = require('./routes');
+
+const app = express();
+const port = 3000;
+
+app.set('trust proxy', 1);
+
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['OjaheRnsshemfnahELF08AEBsjh', 'iajeLSEbdjahwoPEs'],
+  })
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
+app.locals.siteName = 'Roux Meetups';
+
+app.use(express.static(path.join(__dirname, './static')));
+
+app.use(async (req, res, next) => {
+  try {
+    const names = await speakersService.getNames();
+    res.locals.speakerNames = names;
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+});
+
+app.use(
+  '/',
+  routes({
+    feedbackService,
+    speakersService,
+  })
+);
+
+app.use((req, res, next) => {
+  return next(createError(404, 'File not found'));
+});
+
+app.use((error, req, res, next) => {
+  res.locals.message = error.message;
+  console.error(error);
+  const status = error.status || 500;
+  res.locals.status = status;
+  res.status(status);
+
+  res.render('error');
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
