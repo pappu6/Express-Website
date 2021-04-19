89acbb85a0faa602b7c130e0ba0edf141e10c287
Tirth Patel <59409218+pappu6@users.noreply.github.com>
2021-04-19 10:46:05 +0530

    initial commit

---
+++ routes/feedback.js
@@ -0,0 +1,77 @@
+const express = require('express');
+
+const { check, validationResult } = require('express-validator');
+
+const router = express.Router();
+
+const validations = [
+  check('name').trim().isLength({ min: 3 }).escape().withMessage('A name is required'),
+  check('email').trim().isEmail().normalizeEmail().withMessage('A valid email address is required'),
+  check('title').trim().isLength({ min: 3 }).escape().withMessage('A title is required'),
+  check('message').trim().isLength({ min: 5 }).escape().withMessage('A message is required'),
+];
+
+module.exports = (params) => {
+  const { feedbackService } = params;
+
+  router.get('/', async (req, res, next) => {
+    try {
+      const feedback = await feedbackService.getList();
+
+      const errors = req.session.feedback ? req.session.feedback.errors : false;
+      const successMessage = req.session.feedback ? req.session.feedback.message : false;
+      req.session.feedback = {};
+
+      return res.render('layout', {
+        pageTitle: 'Feedback',
+        template: 'feedback',
+        feedback,
+        errors,
+        successMessage,
+      });
+    } catch (error) {
+      return next(error);
+    }
+  });
+
+  router.post('/', validations, async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        req.session.feedback = {
+          errors: errors.array(),
+        };
+        return res.redirect('/feedback');
+      }
+
+      const { name, email, title, message } = req.body;
+      await feedbackService.addEntry(name, email, title, message);
+      req.session.feedback = {
+        message: 'Thank you for your feedback',
+      };
+
+      return res.redirect('/feedback');
+    } catch (error) {
+      return next(error);
+    }
+  });
+
+  router.post('/api', validations, async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.json({ errors: errors.array() });
+      }
+
+      const { name, email, title, message } = req.body;
+      await feedbackService.addEntry(name, email, title, message);
+      const feedback = await feedbackService.getList();
+      return res.json({ feedback, successMessage: 'Thank you for your feedback' });
+    } catch (error) {
+      return next(error);
+    }
+  });
+
+  return router;
+};
---
+++ routes/speakers.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+const router = express.Router();
+
+module.exports = (params) => {
+  const { speakersService } = params;
+
+  router.get('/', async (req, res, next) => {
+    try {
+      const speakers = await speakersService.getList();
+      const artwork = await speakersService.getAllArtwork();
+      return res.render('layout', {
+        pageTitle: 'Speakers',
+        template: 'speakers',
+        speakers,
+        artwork,
+      });
+    } catch (error) {
+      return next(error);
+    }
+  });
+
+  router.get('/:shortname', async (req, res, next) => {
+    try {
+      const speaker = await speakersService.getSpeaker(req.params.shortname);
+      const artwork = await speakersService.getArtworkForSpeaker(req.params.shortname);
+      return res.render('layout', {
+        pageTitle: 'Speakers',
+        template: 'speakers-detail',
+        speaker,
+        artwork,
+      });
+    } catch (error) {
+      return next(error);
+    }
+  });
+
+  return router;
+};
